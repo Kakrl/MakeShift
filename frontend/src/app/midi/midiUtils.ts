e1a3c2cf685c730869a4a0d8c312613c41eb7545
harrydeng104 <harrydeng104@gmail.com>
2026-09-06 12:54:48 -0400

    all basic logic for midi file creation

---
+++ frontend/src/app/midi/midiUtils.ts
@@ -0,0 +1,81 @@
+import MidiWriter from "midi-writer-js";
+
+// fix for typescript note knowing the MidiWriter types
+let track: any = null;
+let recordingStartTime = 0;
+let recordingBpm = 120;
+
+function millisecondsToTicks(
+  milliseconds: number,
+  bpm: number
+): number {
+  return Math.round((milliseconds * 128 * bpm) / 60000);
+}
+
+export function startRecording(bpm: number): void {
+  track = new MidiWriter.Track();
+  track.setTempo(bpm);
+
+  recordingBpm = bpm;
+  recordingStartTime = performance.now();
+}
+
+export function noteOn(
+  pitch: string,
+  velocity: number
+): void {
+  if (track === null) {
+    return;
+  }
+
+  const elapsedTime =
+    performance.now() - recordingStartTime;
+
+  const tick = millisecondsToTicks(
+    elapsedTime,
+    recordingBpm
+  );
+
+  track.addEvent(
+    new MidiWriter.NoteOnEvent({
+      pitch,
+      velocity,
+      tick,
+    })
+  );
+}
+
+export function noteOff(pitch: string): void {
+  if (track === null) {
+    return;
+  }
+
+  const elapsedTime =
+    performance.now() - recordingStartTime;
+
+  const tick = millisecondsToTicks(
+    elapsedTime,
+    recordingBpm
+  );
+
+  track.addEvent(
+    new MidiWriter.NoteOffEvent({
+      pitch,
+      tick,
+    })
+  );
+}
+
+// a bug exists where if the last note was not released when the recording is stopped, it will note be recorded
+export function downloadMidi(): void {
+  if (track === null) {
+    return;
+  }
+
+  const writer = new MidiWriter.Writer(track);
+
+  const link = document.createElement("a");
+  link.href = writer.dataUri();
+  link.download = "recording.mid";
+  link.click();
+}
